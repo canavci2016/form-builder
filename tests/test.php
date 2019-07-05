0b73b8c17e52f4f14c586a3e4919eedb620bb357
can <canavci2017@gmail.com>
2019-07-06 00:18:34 +0300

    initialized form builder

--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
+use FormBuilder\FormBuilder as Form;
+
+$array = ['products' => ['desk' => ['price' => 100]]];
 
 
- print_r(array_dot(['s','ASD','ad']));
+$form = (new Form('cccc', 'POST'))
+    ->inputText('pan', $card['number'])
+    ->inputText('cv2', $card['cv2'])
+    ->inputText('Ecom_Payment_Card_ExpDate_Year', $card['exp_year'])
+    ->inputText('Ecom_Payment_Card_ExpDate_Month', $card['exp_month']);
+
+
+print_r($form);
